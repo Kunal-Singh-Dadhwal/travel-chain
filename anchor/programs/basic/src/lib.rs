93d8170e3254deaa4d4f2bc3c71544d36e5fe364
Hug_Bunter <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-12-16 12:05:59 +0530

    Init Build

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
+declare_id!("7MUhRzYiuxTpkXAPcCDmmBk974HqyedLrf4dpyAFaAGv");
 
 #[program]
 pub mod basic {
